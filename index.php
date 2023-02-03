23c2015343b2cbfaf2dae28f1b6bd80f496ac17e
Geisa Amaral <geisamaral24@gmail.com>
2023-02-03 14:10:06 +0000

    ...

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require __DIR__ ."/vendor/autoload.php";
-    echo "<h1> olá mundo bem vindo ao php </h1>";
-
-
-?>
---
+++ php/email-is-a-table.php
@@ -0,0 +1,29 @@
+<?php
+  //Variáveis
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $date = date('d/m/Y');
+  $time = time('H:i:s');
+  //Compo E-mail
+  $archive = "
+    <html>
+      <p><b>Nome: </b>$name</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>Mensagem: </b>$message</p>
+      <p>Este e-mail foi enviado em <b>$date</b> às <b>$time</b></p>
+    </html>
+  ";
+ 
+  //Emails para quem será enviado o formulário
+  $destino = "reservas@eumamesa.pt";
+  $assunto = "Contato pelo Site";
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $nome <$email>";
+  //Enviar
+  mail($destino, $assunto, $archive, $headers);
+ 
+  echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
+?>

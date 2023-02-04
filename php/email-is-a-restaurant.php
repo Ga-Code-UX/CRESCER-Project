e52e1117ad1897849a7c935a0be7a0f15f177cfa
Geisa Amaral <geisamaral24@gmail.com>
2023-02-04 21:32:08 +0000

    ...

--- php/email-is-a-restaurant.php
+++ php/email-is-a-restaurant.php
@@ -18,7 +18,7 @@
   ";
  
   //Emails para quem será enviado o formulário
-  $destiny= "reservas@eumamesa.pt";
+  $destiny= "reservas@eumrestaurante.pt";
   $subject_Matter = $subject;
   //Este sempre deverá existir para garantir a exibição correta dos caracteres
   $headers  = "MIME-Version: 1.0\n";
@@ -27,5 +27,5 @@
   //Enviar
   mail($destiny, $subject_Matter, $archive, $headers);
  
-  echo "<meta http-equiv='refresh' content='0;URL=../reserva-e-uma-mesa.html'>";
+  echo "<meta http-equiv='refresh' content='0;URL=../reserva-e-uma-esplanada.html'>";
 ?>
